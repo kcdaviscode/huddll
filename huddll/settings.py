c1c945d86c332218a4e3cd0e129304cf10d2b67a
kevin davis <kevindavis@MacBook-Pro-2.local>
2026-02-08 16:07:12 -0500

    Configure database for Railway PostgreSQL

--- huddll/settings.py
+++ huddll/settings.py
@@ -55,11 +55,13 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'huddll.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
